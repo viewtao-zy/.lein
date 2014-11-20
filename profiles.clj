ae9285b8a2bb488ee8bdade7ad6bc14783ff7f22
zhang yuan <zy@viewtao.com>
2014-11-20 12:10:13 +0800

    clojure and clojurescript configuration on ubuntu

---
+++ profiles.clj
@@ -0,0 +1,6 @@
+{:user {:plugins [[lein-difftest "1.3.8"]
+                  [lein-marginalia "0.7.1"]
+                  [lein-pprint  "1.1.1"]
+                  [lein-swank  "1.4.1"]
+                  [cider/cider-nrepl "0.8.0-SNAPSHOT"]
+                  [lib-noir "0.2.0"]]}}
